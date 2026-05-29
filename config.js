9c4b30ef0932c24b43a9ff6b356a50f5e51292b4
ckdeguz <ckdeguz@gmail.com>
2026-05-28 22:35:04 -0400

    Moved hosting from Vercel to Firebase

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 firebase.initializeApp({
-  apiKey: "AIzaSyAm4REUN9J2i4WQUuW2H7jdr6zE0kE06MM",
-  authDomain: "daily-duo-74c8d.firebaseapp.com",
-  databaseURL: "https://daily-duo-74c8d-default-rtdb.firebaseio.com",
-  projectId: "daily-duo-74c8d",
-  storageBucket: "daily-duo-74c8d.firebasestorage.app",
-  messagingSenderId: "51525045037",
-  appId: "1:51525045037:web:2135897e66de4f7cea21a4"
+  apiKey: "AIzaSyCPsSHIfmYi8u1aNIyy_p6g1Mzr3sWJYMA",
+  authDomain: "daily-duo-quiz.firebaseapp.com",
+  databaseURL: "https://daily-duo-quiz-default-rtdb.firebaseio.com",
+  projectId: "daily-duo-quiz",
+  storageBucket: "daily-duo-quiz.firebasestorage.app",
+  messagingSenderId: "684768551867",
+  appId: "1:684768551867:web:da33d5e799a5be31d45a9e",
 });
