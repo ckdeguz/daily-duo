d134a0bd532563ec29f3b8b391d7862889ad070f
ckdeguz <53157061+ckdeguz@users.noreply.github.com>
2026-05-29 15:14:45 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-  apiKey: "AIzaSyCPsSHIfmYi8u1aNIyy_p6g1Mzr3sWJYMA",
-  authDomain: "daily-duo-quiz.firebaseapp.com",
-  databaseURL: "https://daily-duo-quiz-default-rtdb.firebaseio.com",
-  projectId: "daily-duo-quiz",
-  storageBucket: "daily-duo-quiz.firebasestorage.app",
-  messagingSenderId: "684768551867",
-  appId: "1:684768551867:web:da33d5e799a5be31d45a9e",
-});
